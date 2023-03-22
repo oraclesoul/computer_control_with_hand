0c330fc35fd36d0e30d98b158523da5f50559658
oraclesoul <oraclesoul.os@gmail.com>
2023-03-22 21:09:37 +0530

    Model training and testing with gesture recognition

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import cv2 
+from cvzone.HandTrackingModule import HandDetector
 
 #Parameters
 width, height = 360, 360
+
+
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 # Camera Setup
 cap = cv2.VideoCapture(0)
@@ -11,6 +15,7 @@ cap.set(4, height)
 while True:
     success, img = cap.read()
     img = cv2.flip(img,1)
+    hands, img = detector.findHands(img,flipType = False)  # with draw
     cv2.imshow("Camera",img)
     key = cv2.waitKey(1)
     if(key == ord('q')):
